48b29d60f04076da6b8139d0088b2efce17708a0
Simon <Simon@tcpost.dk>
2014-05-07 20:31:45 +0300

    Now also saved

--- myNEAT/src/stcl/myNEAT/evolution/variationOperators/mutators/Mutator.java
+++ myNEAT/src/stcl/myNEAT/evolution/variationOperators/mutators/Mutator.java
@@ -1,5 +1,6 @@
 package stcl.myNEAT.evolution.variationOperators.mutators;
 
+import stcl.myNEAT.evolution.Organism;
 import stcl.myNEAT.evolution.variationOperators.VariationOperator;
 
 public abstract class Mutator implements VariationOperator {
--- myNEAT/src/stcl/myNEAT/evolution/variationOperators/mutators/WeightMutator.java
+++ myNEAT/src/stcl/myNEAT/evolution/variationOperators/mutators/WeightMutator.java
@@ -1,16 +1,30 @@
 package stcl.myNEAT.evolution.variationOperators.mutators;
 
+import java.util.Random;
+
+import stcl.myNEAT.evolution.Organism;
+
 /**
  * This class is responsible for the mutation of connection weights
  * @author Simon
  *
  */
 public class WeightMutator extends Mutator {
-	double minWeight, maxWeight;
-	
+	private double minWeight, maxWeight;
+	private Random rand;	
 	public WeightMutator(double minWeigth, double maxWeight) {
 		this.minWeight = minWeigth;
 		this.maxWeight=maxWeight;
+		rand = new Random();
+	}
+
+	@Override
+	/**
+	 * Mutates all the weights in the organism
+	 */
+	public void mutate(Organism o) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
